b4150c814edb75f514984d63b4b925dff1be45dc
tomroesch <troeschi@caltech.edu>
2020-05-21 23:38:48 -0700

    reduced types to one, test for covfn and kernelmatrix

--- src/gaussianprocesses.jl
+++ src/gaussianprocesses.jl
@@ -1,65 +1,11 @@
 module gaussianprocesses
-export AbstractGaussianProcess, lnGP, nnGP
+
+using LinearAlgebra
+
+export AbstractGaussianProcess, sqexpGP, covfn
 
 abstract type AbstractGaussianProcess end
 
-
-
-"""
-    struct lnGP <: AbstractGaussianProcess
-
-Creates a Gaussian process with linear covariance function.
-
-#Arguments
-- `lthbounds` : a dictionary of pairs of the bounds on the hyperparameters in log10 space, such as Dict(0=> [0,6], 1=> [-3,4], 2=> [-6,-4])
-- `x` : a 1-d array of the abscissa data
-- `y` : a multi-dimensional array of the ordinate data
-- `merrors` : if specified, a 1-d array of the measurement errors (as variances) v, else `nothing`
-- `noparams` : Number of parameters
-"""
-mutable struct lnGP <: AbstractGaussianProcess
-    b
-    x
-    y
-    xnew
-    merror
-    noparams::Int
-    description::String
-    lnGP(lthbounds::Dict, x, y) = new([lthbounds[a] for a in keys(lthbounds)], x, y, x, nothing, 2, "linear gaussian process")
-    lnGP(lthbounds::Dict, x, y, merrors) = new([lthbounds[a] for a in keys(lthbounds)], x, y, x, merror, 2, "linear gaussian process")
-end
-
-
-"""
-    struct nnGP <: AbstractGaussianProcess
-
-Creates a Gaussian process with neural network covariance function.
-
-#Arguments
-- `lthbounds` : a dictionary of pairs of the bounds on the hyperparameters in log10 space, such as Dict(0=> [0,6], 1=> [-3,4], 2=> [-6,-4])
-- `x` : a 1-d array of the abscissa data
-- `y` : a multi-dimensional array of the ordinate data
-- `merrors` : if specified, a 1-d array of the measurement errors (as variances) v, else `nothing`
-- `noparams` : Number of parameters
-"""
-mutable struct nnGP <: AbstractGaussianProcess
-    b
-    x
-    y
-    xnew
-    merror
-    noparams::Int
-    description::String
-    nnGP(lthbounds::Dict, x, y) = new([lthbounds[a] for a in keys(lthbounds)], x, y, x, nothing, 2, "neural network Gaussian process")
-    nnGP(lthbounds::Dict, x, y, merrors) = new([lthbounds[a] for a in keys(lthbounds)], x, y, x, merror, 2, "neural network Gaussian process")
-end
-
-
-function info(self::nnGP)
-    println("hparam[1] determines the initial value.")
-    println("hparam[2] determines the flexibility.")
-    println("hparam[3] determines the variance of the measurement error.")
-end
 
 
 """
@@ -74,17 +20,24 @@ Creates a Gaussian process with squared exponential covariance function.
 - `merrors` : if specified, a 1-d array of the measurement errors (as variances) v, else `nothing`
 - `noparams` : Number of parameters
 """
-mutable struct sqexpGP <: AbstractGaussianProcess
-    b
-    x
-    y
-    xnew
-    merror
+mutable struct sqexpGP{
+    T_b<:AbstractVector,
+    T_x<:AbstractVector,
+    T_y<:AbstractArray,
+    T_xnew<:AbstractVector,
+    T_merror<:AbstractVector
+    } <: AbstractGaussianProcess
+    b::T_b
+    x::T_x
+    y::T_y
+    xnew::T_xnew
+    merrors::T_merror
     noparams::Int
     description::String
-    sqexpGP(lthbounds::Dict, x, y) = new([lthbounds[a] for a in keys(lthbounds)], x, y, x, nothing, 2, "squared exponential Gaussian process")
-    sqexpGP(lthbounds::Dict, x, y, merrors) = new([lthbounds[a] for a in keys(lthbounds)], x, y, x, merror, 2, "squared exponential Gaussian process")
 end
+
+sqexpGP(lthbounds::Dict, x, y) = sqexpGP([lthbounds[a] for a in keys(lthbounds)], x, y, x, Float64[], 2, "squared exponential Gaussian process")
+sqexpGP(lthbounds::Dict, x, y, merrors) = sqexpGP([lthbounds[a] for a in keys(lthbounds)], x, y, x, merrors, 2, "squared exponential Gaussian process")
 
 
 function info(self::sqexpGP)
@@ -95,65 +48,53 @@ end
 
 
 """
-    struct sqexplinGP <: AbstractGaussianProcess
+    covfn(self::sqexpGP, x, xp, lth)
 
-Creates a Gaussian process with squared exponential covariance function with a linear trend.
-
-#Arguments
-- `lthbounds` : a dictionary of pairs of the bounds on the hyperparameters in log10 space, such as Dict(0=> [0,6], 1=> [-3,4], 2=> [-6,-4])
-- `x` : a 1-d array of the abscissa data
-- `y` : a multi-dimensional array of the ordinate data
-- `merrors` : if specified, a 1-d array of the measurement errors (as variances) v, else `nothing`
-- `noparams` : Number of parameters
-"""
-mutable struct sqexplinGP <: AbstractGaussianProcess
-    b
-    x
-    y
-    xnew
-    merror
-    noparams::Int
-    description::String
-    sqexplinGP(lthbounds::Dict, x, y) = new([lthbounds[a] for a in keys(lthbounds)], x, y, x, nothing, 3, "squared exponential Gaussian process with a linear trend")
-    sqexplinGP(lthbounds::Dict, x, y, merrors) = new([lthbounds[a] for a in keys(lthbounds)], x, y, x, merror, 3, "squared exponential Gaussian process with a linear trend")
-end
-
-
-function info(self::sqexplinGP)
-    println("hparam[1] determines the amplitude of variation.")
-    println("hparam[2] determines the flexibility.")
-    println("hparam[3] determines the linear trend with increasing input.")
-    println("hparam[4] determines the variance of the measurement error.")
-end
-
-
-"""
-    struct maternGP <: AbstractGaussianProcess
-
-Creates a Gaussian process with Matern covariance function that is twice differentiable.
+Squared exponential covariance function.
+Returns the kernel function and Jacobian of the kernel function.
 
 #Arguments
-- `lthbounds` : a dictionary of pairs of the bounds on the hyperparameters in log10 space, such as Dict(0=> [0,6], 1=> [-3,4], 2=> [-6,-4])
-- `x` : a 1-d array of the abscissa data
-- `y` : a multi-dimensional array of the ordinate data
-- `merrors` : if specified, a 1-d array of the measurement errors (as variances) v, else `nothing`
-- `noparams` : Number of parameters
+- `self` : gaussianprocess with squared exponential covariance function
+- `x` : a 1-d array of abscissa
+- `xp` : a 1-d array of alternative abscissa
+- `lth` : the log of the hyperparameters
 """
-mutable struct maternGP <:AbstractGaussianProcess
-    b
-    x
-    y
-    xnew
-    merror
-    noparams::Int
-    description::String
-    maternGP(lthbounds::Dict, x, y) = new([lthbounds[a] for a in keys(lthbounds)], x, y, x, nothing, 2, "(twice differentiable) Matern covariance function")
-    maternGP(lthbounds::Dict, x, y, merrors) = new([lthbounds[a] for a in keys(lthbounds)], x, y, x, merror, 2, "(twice differentiable) Matern covariance function")
+function covfn(self::sqexpGP, x, xp, lth)
+    th = exp.(lth)
+    e = exp.(-th[2] / 2.0 * (x .- xp).^2)
+    k = th[1] * e
+    jk = zeros(length(xp), self.noparams)
+    jk[:, 1] = e * th[1]
+    jk[:, 2] = -th[1] * th[2] * e / 2.0 .* (x .- xp).^2
+    return k, jk
 end
 
 
-function info(self::maternGP)
-    println("hparam[1] determines the amplitude of variation.")
-    println("hparam[2] determines the stiffness.")
-    println("hparam[3] determines the variance of the measurement error.")
+"""
+    function kernelmatrix(self::sqexpGP, lth, x)
+
+Returns kernel matrix K(X,X) supplemented with measurement noise and its Cholesky decomposition.
+
+#Arguments
+- `self` : gaussianprocess with squared exponential covariance function
+- `lth` : log of the hyperparameters
+- `x` : abscissa values
+"""
+function kernelmatrix(self::sqexpGP, lth, x)
+    k = zeros(length(x), length(x))
+    for i in 1:length(x)
+        k[i,:]= covfn(self, x[i], x, lth)[1]
+    end
+
+    if ~isempty(self.merrors)
+        kn = k .+ exp(lth[end]) * Diagonal(self.merrors)
+    else
+        kn = k .+ exp(lth[end]) * I(length(x))
+    end
+
+    L = cholesky(kn)
+    return k, L.U
 end
+
+end  # module
+Main.gaussianprocesses
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,33 @@
 include("../src/gaussianprocesses.jl")
-using .gaussianprocesses, Test
+using .gaussianprocesses, Test, LinearAlgebra
 
 @testset "Type structure" begin
-    @test lnGP <: AbstractGaussianProcess
-    @test nnGP <: AbstractGaussianProcess
-    @test sqexpGP <: AbstractGaussianProcess
-    @test sqexplinGP <: AbstractGaussianProcess
-    @test maternGP <: AbstractGaussianProcess
+    @testset "highest level types" begin
+        @test_broken lnGP <: AbstractGaussianProcess
+        @test_broken nnGP <: AbstractGaussianProcess
+        @test sqexpGP <: AbstractGaussianProcess
+        @test_broken sqexplinGP <: AbstractGaussianProcess
+        @test_broken maternGP <: AbstractGaussianProcess
+    end
+    @testset "Infer Field types" begin
+        g1 = sqexpGP(Dict(1=> (-2,2), 2=> (-2,2)), rand(10), rand(10))
+        @test typeof(g1) == sqexpGP{Array{Tuple{Int64,Int64},1},Array{Float64,1},Array{Float64,1},Array{Float64,1},Array{Float64,1}}
+        g2 = sqexpGP(Dict(1=> (-2.,2.), 2=> (-2.,2.)), [1, 2], [1, 2])
+        @test typeof(g2) == sqexpGP{Array{Tuple{Float64,Float64},1},Array{Int64,1},Array{Int64,1},Array{Int64,1},Array{Float64,1}}
+    end
+end
+
+g = sqexpGP(Dict(1=> (-2,2), 2=> (-2,2)), [1, 2], [1, 2])
+
+@testset "squared exponential covariance function" begin
+    out = gaussianprocesses.covfn(g, [1], [1, 2] , [0, 0])
+    @test out[1] == [1, exp(-1/2)]
+    @test out[2] == [1. 0; exp(-1/2) (-1/2*exp(-1 / 2))]
+end
+
+
+@testset "squared exponential covariance function kernel matrix" begin
+    out = gaussianprocesses.kernelmatrix(g, [0, 0], [1, 2])
+    @test out[1] ==  [1 exp(-1/2); exp(-1/2) 1]
+    @test out[2] == cholesky([1+1 exp(-1/2); exp(-1/2) 1+1]).U
 end
